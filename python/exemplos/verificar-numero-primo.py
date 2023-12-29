e47bdb190d617ae9c38a5cf59dfaf65f31607ca2
guibrum147 <guibrum147@outlook.com>
2023-12-29 19:43:14 -0300

    Adição de conteúdo referente a Python

---
+++ python/exemplos/verificar-numero-primo.py
@@ -0,0 +1,12 @@
+while 1 == 1:
+    ctr = 0
+    ctd = 2
+    numero = int(input('Digite o número: '))
+    while ctr == 0 and ctd <= numero**1/2:
+        if numero%ctd == 0:
+            ctr = 1
+        ctd += 1
+    if ctr == 0:
+        print('É um número primo')
+    else:
+        print('Não é um número primo')
